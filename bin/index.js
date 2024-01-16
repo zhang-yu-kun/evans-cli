74dbbccf80acecf9fb7dea1ff1ac6e60f006edc7
Yasin <623502945@qq.com>
2024-01-16 11:01:11 +0800

    创建cli

---
+++ bin/index.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+const program = require('commander')
+const chalk = require('chalk')
+const figlet = require('figlet')
+
+program.command('create <name>').description('创建一个项目')
+  .option('-f, --force', '强制创建，覆盖当前文件夹') // 是否强制创建，当文件夹已经存在
+  .action((name, options) => {
+    require('../lib/create.js')(name, options)
+  })
+
+program
+  // 监听 --help 执行
+  .on('--help', () => {
+    console.log('\r\n' + figlet.textSync('Evans', {
+      font: 'Ghost',
+      horizontalLayout: 'default',
+      verticalLayout: 'default',
+      width: 80,
+      whitespaceBreak: true
+    }));
+
+
+  })
+
+program
+  // 配置版本号信息
+  .version(`v${require('../package.json').version}`)
+  .usage('<command> [option]')
+
+program.parse(process.argv)
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Evans的前端脚手架
+
+## 简介
+这个脚手架主要是个人使用的，两个选项，
+一个是只有一些简单配置的cra
+一个是使用react+vite的前端模板
+
+## 安装
+```
+npm install evans-cli -g
+```
+## 使用
+```
+template create < project >
+```
